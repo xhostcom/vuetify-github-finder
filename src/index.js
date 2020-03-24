ee72ffca8052a5cfc948db8080e7a2ce70427c4b
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-24 22:29:20 +0000

    Update

---
+++ src/index.js
@@ -0,0 +1,4 @@
+function startFunction() {
+    console.log('Index js file does indeed work, put some stuff in it!');
+}
+startFunction();
---
+++ vue.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  devServer: {
+    proxy: 'http://localhost:8080',
+}
+}
+
+/*var webpack = require('webpack');
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+      })
+    ]
+  }
+} if you like extra drama!*/
+
+/* Or more likely*/
+/* vue.config.js
+module.exports = {
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // mutate config for production...
+    } else {
+      // mutate for development...
+    }
+  }
+}*/
+
+
+/*Or Lodash*/
+
+/*var webpack = require('webpack');
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+  _map: ['lodash', 'map']
+      })
+    ]
+  }
+}*/
+
+//or chainWebpack:
